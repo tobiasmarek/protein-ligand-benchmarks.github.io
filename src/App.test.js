9bc684a37d9aa9ffd3046d9b9bf45b4900b4343c
Daniel Konvicka <daniel.bob.dmk@gmail.com>
2025-07-17 23:36:26 -0700

    changed workflow

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+beforeAll(() => {
+  HTMLCanvasElement.prototype.getContext = jest.fn();
+});
+
 test('renders learn react link', () => {
-  render(<App />);
+  render(<App/>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
