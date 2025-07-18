ec327594f78c24b11508308d87134aee5bfe28cb
Daniel Konvicka <daniel.bob.dmk@gmail.com>
2025-07-17 23:47:15 -0700

    changed workflow

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,7 @@ beforeAll(() => {
 });
 
 test('renders learn react link', () => {
-  render(<App/>);
+  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
