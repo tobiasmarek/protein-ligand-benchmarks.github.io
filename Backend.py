b046832fc111c4e5089a41342fb839d39c593e9d
Daniel Konvicka <daniel.bob.dmk@gmail.com>
2025-08-10 14:57:39 -0700

    got rid of logo

--- Backend.py
+++ Backend.py
@@ -5,5 +5,5 @@ from method_results import MethodAcuracy
 data = MethodAcuracy.giveData()  
 with open("public/data.json", "w") as f:
     json.dump(data, f) # indent for pretty-printing
-    print(f)
+    print(data)
 
