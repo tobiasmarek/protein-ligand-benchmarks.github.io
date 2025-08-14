867f1bb9f98c89045e1fcf5e3945d80b4edddc18
Daniel Konvicka <daniel.bob.dmk@gmail.com>
2025-08-13 19:48:50 -0700

    envirnoment

--- Backend.py
+++ Backend.py
@@ -1,9 +1,13 @@
 import json
 from method_results import MethodAcuracy
 
-
-data = MethodAcuracy.giveData()  
-with open("public/data.json", "w") as f:
-    json.dump(data, f) # indent for pretty-printing
-    print(data)
+try: 
+    data = MethodAcuracy.giveData()  
+    with open("public/data.json", "w") as f:
+        json.dump(data, f) # indent for pretty-printing
+        print(data)
+except Exception as e:
+    import traceback
+    traceback.print_exc()
+    exit(1)
 
